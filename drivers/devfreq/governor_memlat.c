bittencourtjulival/eclipse_kernel_xiaomi_stone

PM / devfreq: memlat: increase ratio_ceil limit
Increase the memlat ratio_ceil limit to 50k so
that it can cover a wider range of use cases.

Change-Id: I4785b418e160acc0dabe0ed98146c16336e1e923
Signed-off-by: Amir Vajid <avajid@codeaurora.org>

drivers/devfreq/governor_memlat.c
@@ -320,7 +320,7 @@ static int devfreq_memlat_get_freq(struct devfreq *df,
 }
 
 show_attr(ratio_ceil);
-store_attr(ratio_ceil, 1U, 20000U);
+store_attr(ratio_ceil, 1U, 50000U);
 static DEVICE_ATTR_RW(ratio_ceil);
 show_attr(stall_floor);
 store_attr(stall_floor, 0U, 100U);

TwoMinus-Source/stellaris_kernel_oneplus_sm8250

drivers/devfreq/governor_memlat.c
@@ -325,7 +325,7 @@ static int devfreq_memlat_get_freq(struct devfreq *df,
 	return 0;
 }
 
-gov_attr(ratio_ceil, 1U, 20000U);
+gov_attr(ratio_ceil, 1U, 50000U);
 gov_attr(stall_floor, 0U, 100U);
 gov_attr(wb_pct_thres, 0U, 100U);
 gov_attr(wb_filter_ratio, 0U, 50000U);