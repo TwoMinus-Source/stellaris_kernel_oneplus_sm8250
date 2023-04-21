cd8dccb467bc4c60c39000ef77d0bc0cd9b60d77
dawfuk <dawfukfr@gmail.com>
2023-04-21 23:35:54 +0200

    kgsl : fix unused variable
    ../drivers/gpu/msm/kgsl_events.c:50:6: warning: unused variable 'id' [-Wunused-variable]
            int id = KGSL_CONTEXT_ID(event->context);
    
    Signed-off-by: dawfuk <dawfukfr@gmail.com>

--- drivers/gpu/msm/kgsl_events.c
+++ drivers/gpu/msm/kgsl_events.c
@@ -47,7 +47,6 @@ const char *prio_to_string(enum kgsl_priority prio)
 static void _kgsl_event_worker(struct work_struct *work)
 {
 	struct kgsl_event *event = container_of(work, struct kgsl_event, work);
-	int id = KGSL_CONTEXT_ID(event->context);
 
 	event->func(event->device, event->group, event->priv, event->result);
 
